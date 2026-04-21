d9104b253a6cd077f57c43472e453f6f2af69ca0
Amelia Huge <ah2252@g.rit.edu>
2026-04-21 18:29:34 -0400

    video changes

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,12 +1,12 @@
 import Container from '@mui/material/Container'
-// import vid from '../videos/ImagineRIT_Vid_2025_NoCaption.mp4'
+import vid from '/public/videos/ImagineRIT_Vid_2025_NoCaption.mp4'
 
 
 export default function Video() {
   return (
 
         <video width="100%" height="450px" controls>
-            <source src='/src/videos/ImagineRIT_Vid_2025_NoCaption.mp4' type="video/mp4"/>
+            <source src={vid} type="video/mp4"/>
         </video>
 
   );

9151849d6b670ffb1408a56ae60408429e6b42ac
Amelia Huge <ah2252@g.rit.edu>
2026-04-23 14:16:38 -0400

    search bar shadow

--- quacknet-next/src/app/components/SearchBar.tsx
+++ quacknet-next/src/app/components/SearchBar.tsx
@@ -10,7 +10,7 @@ export default function CustomizedInputBase() {
   return (
     <Paper
       component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, background: 'rgba(131, 135, 133, 0.55)', border: '1px solid #838785' }}
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, background: 'rgba(131, 135, 133, 0.55)', border: '1px solid #838785', boxShadow: 'inset 2px -1px 4px 4px rgba(0, 0, 0, 0.2)' }}
     >
       <IconButton sx={{ p: '10px', color: 'white' }} aria-label="search">
         <SearchIcon />

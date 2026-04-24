960f926c3f75de8fd3dfdced76588504e303bc74
Amelia Huge <ah2252@g.rit.edu>
2026-04-23 20:26:12 -0400

    postgres stuff

--- quacknet-next/src/app/api/games/route.ts
+++ quacknet-next/src/app/api/games/route.ts
@@ -1,9 +1,11 @@
-import db from '@/lib/db'
+import postgres from 'postgres'
 import { NextResponse } from 'next/server'
+
+const sql = postgres(process.env.DATABASE_URL!)
 
 export async function GET() {
   try {
-    const games = db.prepare('SELECT * FROM Game').all();
+    const games = await sql`SELECT * FROM "Game"`;
     console.log('Games from DB:', games);
     return NextResponse.json(games);
   } catch (err) {
